51b889548b0302b82b41e154e02a128ed59e2367
Dima1506 <ichetkin.d@haclever.org>
2021-12-03 17:30:40 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,51 @@
+from typing import Optional
+import requests
+from fastapi import FastAPI
+
+import gensim
+import nltk
+import pandas as pd
+import re
+import urllib.request
+import numpy as np
+import pymorphy2
+import pandas as pd
+from gensim.models import Word2Vec
+from nltk.corpus import stopwords
+from nltk import FreqDist
+from sklearn.manifold import TSNE
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+
+app = FastAPI()
+
+
+@app.get("/login")
+def login():
+    return {"url": "https://tvscp.tionix.ru/realms/master/protocol/openid-connect/auth?response_type=code&grant_type=authorization_code&client_id=tvscp&scope=openid&redirect_uri=https://oauth.pstmn.io/v1/callback"}
+
+
+@app.post("/get_token")
+def get_token(code: str):
+    url = 'https://tvscp.tionix.ru/realms/master/protocol/openid-connect/token'
+    payload = {
+        'client_id': 'tvscp',
+        'client_secret': 'f3e94369-53ac-43d5-842e-09fe6d8a71ff',
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri':'https://oauth.pstmn.io/v1/callback'
+    }
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    r = requests.post(url, data=payload, headers=headers)
+    #print(r.text)
+    token = eval(r.text)['access_token']
+    print(token)
+    return {"token": token}
+
+@app.post("/info_user")
+def info_user(token: str):
+    url = 'https://tvscp.tionix.ru/realms/master/protocol/openid-connect/userinfo'
+    headers={'Authorization':'Bearer '+str(token)}
+    r = requests.post(url, headers=headers)
+    return eval(r.text)
